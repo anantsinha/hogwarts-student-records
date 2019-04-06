a4728c46f686c9b410565d268575b570915fd31b
Anant Sinha <anantsinha9811@gmail.com>
2019-04-06 17:03:59 +0530

    Ready for submission

---
+++ server.js
@@ -0,0 +1,130 @@
+const express = require('express');
+const server = express();
+server.use(express.json());
+var MongoClient = require('mongodb').MongoClient;
+const port = process.env.port || 5000;
+const bodyParser = require("body-parser");
+
+server.use(bodyParser.urlencoded({ extended: false }));
+server.use(bodyParser.json());
+const db_uri = "mongodb+srv://anant:218374218374@cluster0-wded1.mongodb.net/test?retryWrites=true"; //my db
+let client;
+let db;
+
+const connectToDb = async () => {
+  if (client == undefined){
+    try{
+      client = await MongoClient.connect(db_uri, { useNewUrlParser: true });
+      db = client.db('hogwarts');
+    } catch(err){
+      console.log('Error while connecting to database\n', err);
+    }
+  }
+}
+
+connectToDb();
+
+const isValidStudent  = (student) => {
+  let name = student.name;
+  let house = student.house;
+  let year = student.year;
+  let gender = student.gender;
+  let title = student.title;
+  if (name==undefined||house==undefined||year==undefined||gender==undefined)
+    return false;
+  else
+    return true;
+}
+
+
+
+/**************************** ROUTES *********************/
+server.get ('/', (req,res) => {
+	res.send ('Welcome to Hogwarts School of Witchcraft and wizardry Student Data records.'+
+  'The muggles have made it easy for us to keep track of our students');
+	});
+
+server.get('/students', async (req,res) => {
+      let studentList = [];
+        const students = db.collection('students');
+        try{
+          if (req.body.s_id == undefined){
+            studentList = await students.find().toArray();
+          } else {
+            studentList = await students.find({s_id: req.body.s_id}).toArray();
+          }
+        } catch(err){
+            console.log('Error while accessing all students\n',err);
+            res.status(400);
+          } finally {
+            res.send(studentList);
+          }
+  });
+
+  server.post('/students/update', async (req,res) => {
+    student = {};
+    const students = db.collection('students');
+      try{
+        student = await students.findOne({s_id: req.body.s_id});
+        //find details that differ
+        //change those details
+        students.updateOne(
+          {s_id: student.s_id},
+          {
+            $set: req.body
+          }
+        );
+
+      } catch(err){
+        console.log('Error while updating student\n', err);
+        res.status(400);
+      } finally {
+        res.send('Updated')
+      }
+
+  });
+
+server.post('/students/newstudent', async (req,res) => {
+  if (isValidStudent(req.body)){
+      db.collection('students').insertOne(req.body);
+      res.status(200);
+      res.send('object added');
+}
+  else{
+    res.status(400);
+    res.send("missing required properties");
+  }
+});
+
+server.post('/students/delete', async (req,res) => {
+  let toDelete = req.body;
+  let deleted = {};
+  let status;
+    try{
+      let exists = await db.collection('students').find({s_id: req.body.s_id}).toArray();
+      if (exists.length==0){
+        status = 404;
+        console.log('Attempt to delete non-existent student\n\n',req.body);
+      }
+      await db.collection('students').deleteOne(req.body);
+    } catch(err){
+      console.log('Error while deleting student\n',err);
+      status = 404;
+    } finally {
+      if (status==undefined){
+        status = 204;
+      }
+      res.status(status);
+      res.send('Deleted');
+    }
+  });
+
+
+//send 404 for invalid routes
+  server.use((req, res) => {
+     res.sendStatus(404);
+  });
+
+server.listen(port, () => {
+  console.log(`\nServer listening on port: ${port}\n`);
+});
---
+++ test/test.js
@@ -0,0 +1,251 @@
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+let qs = require('querystring');
+var MongoClient = require('mongodb').MongoClient;
+chai.use(chaiHttp);
+const expect = chai.expect;
+
+const db_uri = "mongodb+srv://anant:218374218374@cluster0-wded1.mongodb.net/test?retryWrites=true"; //my db
+let client;
+let db;
+let students;
+
+
+describe('Students', async () => {
+	before ( async () => {
+		//connect to database
+		if (client == undefined){
+			try{
+				client = await MongoClient.connect(db_uri, { useNewUrlParser: true });
+				db = client.db('hogwarts');
+				students = db.collection('students');
+			} catch(err){
+				console.log('Error while connecting to database\n', err);
+			}
+		}
+	});
+
+	describe('/POST /students/update', async () => {
+		it ("Should change name when correct id is passed", async () => {
+			let res;
+			try{
+				res = await chai.request('http://localhost:5000').post('/students/update').set('content-type', 'application/x-www-form-urlencoded')
+				.send(qs.stringify({ s_id: '3', name: 'Ron'}));
+			} catch (err) {
+				throw err;
+			} finally{
+					expect(res.status).to.equal(200);
+			}
+		});
+
+		it ("Should not change name when incorrect id is passed", async () => {
+			let res;
+			try{
+				res = await chai.request('http://localhost:5000').post('/students/update').set('content-type', 'application/x-www-form-urlencoded')
+				.send(qs.stringify({ s_id: '23', name: 'Ron'}));
+			} catch (err) {
+				throw err;
+			} finally{
+					expect(res.status).to.equal(400);
+			}
+		});
+	});
+
+
+	describe('/POST /students/delete', async () => {
+			it ("Should delete existing student", async () => {
+							try{
+								await populateDatabase();
+							} catch(err){
+								throw err;
+							}
+							finally {
+								try{
+									res = await chai.request('http://localhost:5000').post('/students/delete').set('content-type', 'application/x-www-form-urlencoded')
+									.send(qs.stringify({ s_id: '3', name: 'Ronald'}));
+								} catch (err){
+									throw err;
+								}
+								finally {
+									expect(res.status).to.equal(204);
+								}
+							}
+			});
+
+
+		it ("Should fail to delete non-existing student", async () => {
+			let res;
+			try{
+				res = await chai.request('http://localhost:5000').post('/students/delete').set('content-type', 'application/x-www-form-urlencoded')
+				.send(qs.stringify({ s_id: '23', name: 'Ron'}));
+			} catch (err) {
+				throw err;
+			} finally{
+					expect(res.status).to.equal(404);
+			}
+		});
+	});
+
+	describe('/POST /students/newstudent', async () => {
+		it ("Should add a new student when all details are given", async () => {
+			let res;
+			try{
+				res = await chai.request('http://localhost:5000').post('/students/newstudent').set('content-type', 'application/x-www-form-urlencoded')
+				.send(qs.stringify({ 	s_id: '3',
+  														name: 'Cho',
+  														gender: 'Witch',
+  														title: 'Prefect',
+  														house: 'Ravenclaw',
+  														year: '4'}));
+			} catch (err) {
+				throw err;
+			} finally{
+					expect(res.status).to.equal(200);
+			}
+		});
+
+
+		it ("Should not add a new student when required details are not given", async () => {
+			let res;
+			try{
+				res = await chai.request('http://localhost:5000').post('/students/newstudent').set('content-type', 'application/x-www-form-urlencoded')
+				.send(qs.stringify({ 	s_id: '3',
+  														gender: 'Witch',
+  														title: 'Prefect',
+  														house: 'Ravenclaw',
+  														year: '4'}));
+			} catch (err) {
+				throw err;
+			} finally{
+					expect(res.status).to.equal(400);
+			}
+		});
+
+	});
+
+  describe('/GET students', () => {
+    it('should GET all the students', async ()  => {
+			try{
+				await students.deleteMany({});
+				await populateDatabase();
+			} catch (err) {
+				throw err;
+			}
+			let res;
+			try{
+      	res = await chai.request('http://localhost:5000').get('/students');
+			} catch (err) {
+				throw err;
+			}
+			let expected = [
+				    {
+				        "s_id": "1",
+				        "name": "Harry Potter",
+				        "gender": "Wizard",
+				        "title": "Quiddich Captain",
+				        "house": "Gryffindor",
+				        "year": "5"
+				    },
+				    {
+				        "s_id": "2",
+				        "name": "Hermoine Granger",
+				        "gender": "Witch",
+				        "title": "Prefect",
+				        "house": "Gryffindor",
+				        "year": "5"
+				    },
+				    {
+				        "s_id": "3",
+				        "name": "Ronald Weasley",
+				        "gender": "Wizard",
+				        "title": "Prefect",
+				        "house": "Gryffindor",
+				        "year": "5"
+				    },
+				    {
+				        "s_id": "4",
+				        "name": "Ginerva Molly Weasley",
+				        "gender": "Witch",
+				        "title": "",
+				        "house": "Gryffindor",
+				        "year": "4"
+				    },
+				    {
+				        "s_id": "5",
+				        "name": "Draco Malfoy",
+				        "gender": "Wizard",
+				        "title": "Prefect",
+				        "house": "Slytherin",
+				        "year": "4"
+				    }
+				];
+			//_id is automatically added by mongo each time a new student is added
+			//it must be removed before checking equality
+ 			for (let i in res.body){
+	 			delete res.body[i]._id;
+ 			}
+	   expect(res.body).to.eql(expected);
+    });
+  });
+});
+
+
+const populateDatabase = () => {
+	let harry = {
+			"s_id": "1",
+			"name": "Harry Potter",
+			"gender": "Wizard",
+			"title": "Quiddich Captain",
+			"house": "Gryffindor",
+			"year": "5"
+	};
+
+	let hermoine = {
+			"s_id": "2",
+			"name": "Hermoine Granger",
+			"gender": "Witch",
+			"title": "Prefect",
+			"house": "Gryffindor",
+			"year": "5"
+	};
+
+	let ron = {
+			"s_id": "3",
+			"name": "Ronald Weasley",
+			"gender": "Wizard",
+			"title": "Prefect",
+			"house": "Gryffindor",
+			"year": "5"
+	};
+
+	let ginny = {
+			"s_id": "4",
+			"name": "Ginerva Molly Weasley",
+			"gender": "Witch",
+			"title": "",
+			"house": "Gryffindor",
+			"year": "4"
+	};
+
+	let draco = {
+			"s_id": "5",
+			"name": "Draco Malfoy",
+			"gender": "Wizard",
+			"title": "Prefect",
+			"house": "Slytherin",
+			"year": "4"
+	};
+	students.insertMany([harry, hermoine, ron, ginny, draco]);
+}
+
+describe('/GET invalidpath', () => {
+	it ('Should return 404 for non existent path', async () =>{
+		try{
+			res = await chai.request('http://localhost:5000').get('/invalid');
+		} catch (err) {
+			throw err;
+		} finally{
+			expect(res.status).to.equal(404);
+		}
+	});
+});
